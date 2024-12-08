ac1147e2045218d2833ed4b20667feafd1004c42
Grzegorz Turek <g.turek@yahoo.com>
2024-12-08 12:53:55 +0100

    view functions

--- biblioteka/views.py
+++ biblioteka/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from models import Title, Author, Genre
 
-# Create your views here.
+def title_list(request):
+    titles = Title.objects.all()
+    return render(request, 'biblioteka/title_list.html', {'titles': titles})
+
+def title_detail(request, pk):
+    title = get_object_or_404(Title, pk=pk)
+    return render(request, 'biblioteka/title_detail.html', {'title': title})
